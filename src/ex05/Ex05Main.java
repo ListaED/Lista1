e762c93a3b4b33d61793cfb3b4e11c50e1eb1021
pachecogu <pacheco.gu@gmail.com>
2019-09-18 22:25:57 -0300

    Refatoração

--- src/ex05/Ex05Main.java
+++ src/ex05/Ex05Main.java
@@ -2,10 +2,10 @@ package ex05;
 
 import static ex05.Ordenacao.bubbleSortCrescenteVetor;
 import static ex05.Busca.buscaBinaria;
-import static ex05.Busca.buscaNumeroRepeticoes;
 import static ex05.Busca.buscaSequencial;
 import static ex05.Busca.verificarPar;
 import java.util.Scanner;
+import static ex05.Busca.buscaRepeticoes;
 
 public class Ex05Main {
 
@@ -13,21 +13,16 @@ public class Ex05Main {
 
         int n = 15;
         int vet[] = new int[n];
-        int i, num, np;
+        int np;
         int existe = 1;
-        
-        //##################################################################################################
 
-        //leitura dos números para guardar no vet
+        int local;
         try (Scanner scanner = new Scanner(System.in)) {
-            for (i = 0; i < n; i++) {
+            for (int i = 0; i < n; i++) {
                 while (existe == 1) {
-                    
                     System.out.println("Digite o " + (i + 1) + " º número: ");
-                    num = scanner.nextInt();
-
-                    existe = buscaNumeroRepeticoes(vet, num);
-
+                    int num = scanner.nextInt();
+                    existe = buscaRepeticoes(vet, num);
                     if (existe == 1) {
                         System.out.println(" O número digitado já existe. Digite outro!");
                     } else {
@@ -35,59 +30,42 @@ public class Ex05Main {
                     }
                 }
                 existe = 1;
-            }
-            scanner.close();
-        }
-
-        //##################################################################################################
-        
-        //ORDENANDO O VETOR
-        bubbleSortCrescenteVetor(vet);
-
-        //##################################################################################################
-
-        //SELECIONANDO O NUMERO DESEJADO
-        try (Scanner sc = new Scanner(System.in)) {
+            }   
+            //##################################################################################################
+            //ORDENANDO O VETOR
+            bubbleSortCrescenteVetor(vet);
+            //##################################################################################################
+            //SELECIONANDO O NUMERO DESEJADO
             System.out.println("Digite o número desejado: ");
-            np = sc.nextInt();
-            sc.close();
-        }
-        
-        //BUSCANDO O INDICE DO VETOR QUE CONTEM O NUMERO DESEJADO
-        int local = buscaSequencial(vet, np);
-        
-        local++; // Convertendo para uma posição amigavel ao usuário de acordo com a sequencia digitada.
-        
-        //EXIBINDO O INDICE DO VETOR QUE CONTEM O NUMERO DESEJADO
-        System.out.println("Pela busca sequencial,o número desejado se encontra na posição: " + local);
-        
-        if(verificarPar(local)){
-            System.out.println("Esta em uma posição par");
-        }else{
-            System.out.println("Esta em uma posição impar");
-        }
-        
-        //##################################################################################################
-        
-        //SELECIONANDO O NUMERO DESEJADO
-        try (Scanner sc = new Scanner(System.in)) {
+            np = scanner.nextInt();
+            //BUSCANDO O INDICE DO VETOR QUE CONTEM O NUMERO DESEJADO
+            local = buscaSequencial(vet, np);
+            local++; // Convertendo para uma posição amigavel ao usuário de acordo com a sequencia digitada.
+            //EXIBINDO O INDICE DO VETOR QUE CONTEM O NUMERO DESEJADO
+            System.out.println("Pela busca sequencial,o número desejado se encontra na posição: " + local);
+            if (verificarPar(local)) {
+                System.out.println("Esta em uma posição par");
+            } else {
+                System.out.println("Esta em uma posição impar");
+            }   //##################################################################################################
+            //SELECIONANDO O NUMERO DESEJADO
             System.out.println("Digite o número desejado: ");
-            np = sc.nextInt();
-            sc.close();
+            np = scanner.nextInt();
         }
-        
+
         //BUSCANDO O INDICE DO VETOR QUE CONTEM O NUMERO DESEJADO
         local = buscaBinaria(vet, np);
-        
+
+        local++; // Convertendo para uma posição amigavel ao usuário de acordo com a sequencia digitada.
+
         //EXIBINDO O INDICE DO VETOR QUE CONTEM O NUMERO DESEJADO
         System.out.println("Pela busca binária,o número desejado se encontra na posição: " + local);
-        
-        if(verificarPar(local)){
+
+        if (verificarPar(local)) {
             System.out.println("Esta em uma posição par");
-        }else{
+        } else {
             System.out.println("Esta em uma posição impar");
-        }        
-        
-   
+        }
+
     }
 }
