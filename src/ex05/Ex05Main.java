2673b93e871f224a0e9b8f9a266755c2e8e799a8
pachecogu <pacheco.gu@gmail.com>
2019-09-23 06:36:44 -0300

    Ex05_atualizado
    Inclusão de laço na busca sequencial

--- src/ex05/Ex05Main.java
+++ src/ex05/Ex05Main.java
@@ -11,20 +11,19 @@ public class Ex05Main {
 
     public static void main(String[] args) {
 
-        int n = 15;
+        int n = 4;
         int vet[] = new int[n];
         int i, num, np;
-        int existe = 1;
-        
-        //##################################################################################################
+        int existe = 0;
 
+        //##################################################################################################
         //leitura dos números para guardar no vet
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
             for (i = 0; i < n; i++) {
                 while (existe == 1) {
                     
                     System.out.println("Digite o " + (i + 1) + " º número: ");
-                    num = sc.nextInt();
+                    num = scanner.nextInt();
 
                     existe = buscaNumeroRepeticoes(vet, num);
 
@@ -36,21 +35,18 @@ public class Ex05Main {
                 }
                 existe = 1;
             }
-           
-        
 
         //##################################################################################################
-        
         //ORDENANDO O VETOR
         bubbleSortCrescenteVetor(vet);
-        
+
         //##################################################################################################
         //SELECIONANDO O NUMERO DESEJADO
         boolean repete = true;
         while (repete) {
             System.out.println("\nBUSCA SEQUENCIAL: ");
             System.out.println("Digite o número desejado: ");
-            np = sc.nextInt();
+            np = scanner.nextInt();
 
             //BUSCANDO O INDICE DO VETOR QUE CONTEM O NUMERO DESEJADO
             int local = buscaSequencial(vet, np);
@@ -69,33 +65,28 @@ public class Ex05Main {
 
             }
         }
-        
+
         //##################################################################################################
         //SELECIONANDO O NUMERO DESEJADO
-        repete = true;
-        while (repete) {
-            System.out.println("\nBUSCA BINARIA: ");
-            System.out.println("Digite o número desejado: ");
-            np = sc.nextInt();
+        System.out.println("Digite o número desejado: ");
+        np = scanner.nextInt();
 
-            //BUSCANDO O INDICE DO VETOR QUE CONTEM O NUMERO DESEJADO
-            int local = buscaBinaria(vet, np);
-            if (local == -1) {
-                System.out.println("Número não localizado!");
+        //BUSCANDO O INDICE DO VETOR QUE CONTEM O NUMERO DESEJADO
+        int local = buscaBinaria(vet, np);
+        if (local == -1) {
+            System.out.println("Número não localizado!");
+        } else {
+
+            //EXIBINDO O INDICE DO VETOR QUE CONTEM O NUMERO DESEJADO
+            System.out.println("Pela busca binária,o número desejado se encontra na posição: " + local + " no vetor.");
+
+            if (verificarPar(local)) {
+                System.out.println("Esta em uma posição par");
             } else {
-                repete = false;
-                //EXIBINDO O INDICE DO VETOR QUE CONTEM O NUMERO DESEJADO
-                System.out.println("Pela busca binária,o número desejado se encontra na posição: " + local + " no vetor.");
-
-                if (verificarPar(local)) {
-                    System.out.println("Esta em uma posição par");
-                } else {
-                    System.out.println("Esta em uma posição impar");
-                }
-
+                System.out.println("Esta em uma posição impar");
             }
-        }     
-        
-   
+
+        }
+
     }
 }
